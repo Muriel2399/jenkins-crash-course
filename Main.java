bb8dfb85ea61ce02b64c5586dbef393ceea061c5
Muriel Samira Pinto <murielspinto@gmail.com>
2026-04-06 14:22:47 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,8 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Main");
+        System.out.println("Hello Main start *****************");
         System.out.println(new Date());
+        System.out.println("Hello Main end **************");
     }
 }
