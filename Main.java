0e4055faf26a794e065610f8984d2b31f0c7d20c
Muriel Samira Pinto <murielspinto@gmail.com>
2026-04-06 14:25:38 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Main start *****************");
         System.out.println(new Date());
+        Student student = new Student();
+        student.sayHello();
         System.out.println("Hello Main end **************");
     }
 }
