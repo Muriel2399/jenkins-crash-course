8aed7b35107c401a5d71dc5d369ed8cd729f9143
Muriel Samira Pinto <murielspinto@gmail.com>
2026-04-06 14:16:22 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Main");
+        System.out.println(new Date());
+    }
+}
